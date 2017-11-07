c8732a80d458ccf807e957c6c2df9692f77375ee
Coleman Nugent <colemannugent@gmail.com>
2017-11-06 23:25:46 -0800

    gnucash2ledger.py: First complete iteration
    As of this commit gnucash2ledger actually delivers on its promises. Now
    I just have to clean all this up so that I can understand it when it
    breaks in a few months.

--- gnucash2ledger.py
+++ gnucash2ledger.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
 from Split import *
 from Account import *
@@ -12,9 +13,11 @@ from Utils import *
 # Create our argument parser object
 parser = argparse.ArgumentParser(description='Convert gnucash files to ledger files')
 
+# TODO: Add arguments for output to file or to stdout
 # Add our arguments to the parser
 parser.add_argument("file", help="Path to the file to convert")
 
+# TODO: Check to see if the file is gzipped and take care of it
 # Make sure we have enough arguments, if not print the help message
 if len(sys.argv) < 2:
         parser.print_help()
@@ -30,23 +33,36 @@ print("File:", args.file)
 tree = ET.parse(args.file)
 root = tree.getroot()
 
+# list of all accounts
+accounts = []
+
 # iterate through all accounts
 for account in root.iterfind('gnc:book/gnc:account', ns):
 	temp = parse_account(account)
-	temp.display()
-	print()
+	accounts.append(temp)
 
-# iterate through all transactions
+# TODO: Pre-generate accounts at top of ledger file and impose assertions: http://ledger-cli.org/3.0/doc/ledger3.html#Command-Directives
+
+print("Found", len(accounts), "accounts")
+
+# Obtain the full name for each account
+for account in accounts:
+	account.fullname = get_fullname(account, accounts)
+
+# Display the root account
+#accounts[0].display()
+
+# iterate through all transactions and build a list
+transactions = []
 for transaction in root.iterfind('gnc:book/gnc:transaction', ns):
 	temp = parse_transaction(transaction)
-	temp.display()
-	print()
+	temp.date = datetime.strptime(temp.date_posted, "%Y-%m-%d %H:%M:%S %z")
+	transactions.append(temp)
 
-# TODO:
-# Iterate through all accounts
-#	- Generate a list of Account objects
-#	- Be able to translate guids to account names
-# Sort transactions by time
-#	- Which time? 
-# Start building translation layer to ledger format
-#	- Identify main parts of ledger format
+# Sort transactions by date
+#for transaction in transactions:
+	#transaction.display()
+
+transactions.sort(key=lambda item: item.date)
+for transaction in transactions:
+	translate_to_ledger(transaction, accounts)
